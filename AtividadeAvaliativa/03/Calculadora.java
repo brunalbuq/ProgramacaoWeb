39b8ff0e8c829aa0157dd2e968f15c30bd70c050
Bruna Albuquerque <70643277+brunalbuq@users.noreply.github.com>
2021-09-03 23:36:30 -0300

    Create Calculadora.java

---
+++ AtividadeAvaliativa/03/Calculadora.java
@@ -0,0 +1,58 @@
+package Exercicio3Avaliativo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Calculadora
+ */
+@WebServlet("/Calculadora")
+public class Calculadora extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Calculadora() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String svalorA = request.getParameter("valorA");
+        String svalorB = request.getParameter("valorB");
+        String sopcao = request.getParameter("opcao");
+
+        double dvalorA = Double.parseDouble(svalorA);
+        double dvalorB = Double.parseDouble(svalorB);
+        double resultado = 0;
+
+        if(sopcao.equals("+")) {
+            resultado = dvalorA + dvalorB; 
+        }
+        else if(sopcao.equals("-")) {
+            resultado = dvalorA - dvalorB; 
+        }
+        else if(sopcao.equals("*")) {
+            resultado = dvalorA * dvalorB; 
+        }
+        else if(sopcao.equals("/")) {
+            resultado = dvalorA / dvalorB; 
+        }
+
+        out.print("Resultado:<br>" + resultado);
+
+    }
+	
+}
