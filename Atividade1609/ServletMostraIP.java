e2c41c3bebf6e30b546948d7431321554d35ecba
Bruna Albuquerque <70643277+brunalbuq@users.noreply.github.com>
2021-09-22 20:03:01 -0300

    Create ServletMostraIP.java

---
+++ Atividade1609/ServletMostraIP.java
@@ -0,0 +1,46 @@
+package Aula1609;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletMostraIP
+ */
+@WebServlet("/ServletMostraIP")
+public class ServletMostraIP extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletMostraIP() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter resposta = response.getWriter();
+		
+		resposta.write("IP do cliente: ");
+		resposta.write(request.getRemoteAddr());
+		resposta.write("Host do cliente: ");
+		resposta.write(request.getRemoteHost());
+	}
+	
+
+}
